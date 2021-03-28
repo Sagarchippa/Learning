9a1f8ca3d8b37aea65eaeeef1304608de5e4ec1b
Sagarchippa <38092387+Sagarchippa@users.noreply.github.com>
2021-03-28 10:26:02 +0530

    some modifications done

--- namaste.java
+++ namaste.java
@@ -1,7 +1,9 @@
 class First
 {
   public static void main(String[] args){
+    System.out.println("idi java");
     System.out.println("namaste");
     System.out.println("arey entra idi");
+    System.out.println("confusing");
   }
 }
